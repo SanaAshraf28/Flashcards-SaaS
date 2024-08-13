f8131e3775f3b16a42e7482e78e95502f4bfc70f
Sana Muhammad Ashraf <ashras17@mcmaster.ca>
2024-08-13 01:56:05 -0400

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,136 @@
+"use client"
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import getStripe from "@/utils/get-stripe";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Head from "next/head";
+import { experimentalStyled as styled } from '@mui/material/styles';
+import { Grid, Paper, Box, Button, Container, AppBar, Toolbar, Typography } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Home() {
+  const handleSubmit = async ()=>{
+    const checkoutSession = await fetch('/api/checkout_sessions', {
+      method: 'POST',
+      headers: {
+        origin: 'https://localhsot:3000',
+      }
+    })
+
+    const checkoutSessionJson = await checkoutSession.json()
+
+    if (checkoutSession.statusCode === 500) {
+      console.error(checkoutSession.message)
+      return
+    }
+
+    const stripe = await getStripe
+    const {error} = await stripe.redirectToCheckout({
+      sessionId: checkoutSessionJson.id,
+    })
+
+    if (error) {
+      console.warn(error.message)
+    }
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Container maxWidth='lg'>
+      <Head>
+        <title>
+          Flashcard Saas
+        </title>
+        <meta name="description" content = "Create flashcard from your text" />
+      </Head>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant = "h6" style={{flexGrow:1}}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in"> Login </Button>
+            <Button color="inherit" href="/sign-up"> Register </Button>
+          </SignedOut>
+        </Toolbar>
+      </AppBar>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Box
+        sx={({
+          textAlign: "center",
+          my:4,
+        })}>
+        <Typography variant = "h3"> Welcome to Flashcard SaaS</Typography>
+        <Typography variant="h5">
+          Build your flashcards today!
+        </Typography>
+        <Button variant="contained" color="primary" sx={{at: 2}}>
+          Get Started
+        </Button>
+      </Box>
+      <Box sx = {{my: 6}}>
+        <Typography variant="h4" components="h2" align="center">
+          Key Features
+        </Typography>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={4}>
+            <Item><Typography variant="h6">
+              Feature 1
+            </Typography>
+            <Typography> {' '} Input your materials and get your flashcards today!</Typography></Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item><Typography variant="h6">
+              Feature 2
+            </Typography>
+            <Typography> {' '} Input your materials and get your flashcards today!</Typography></Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item><Typography variant="h6">
+              Feature 3
+            </Typography>
+            <Typography> {' '} Input your materials and get your flashcards today!</Typography></Item>
+          </Grid>
+        </Grid>
+      </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+     
+      <Box sx = {{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4">
+          Pricing
+        </Typography>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} alignItems={"center"}>
+          <Grid item xs={6}>
+            <Item><Typography variant="h6" gutterBottom>
+              Basic Plan
+            </Typography>
+            <Typography> {' '} Access to basic features and limited storage</Typography>
+            <Button variant="contained" color="secondary" sx={{mt:2}}>
+              Go Basic
+            </Button>
+            </Item>
+          </Grid>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+          <Grid item xs={6}>
+            <Item><Typography variant="h6" gutterBottom>
+              Premium
+            </Typography>
+            <Typography> {' '} Input your materials and get your flashcards today!</Typography>
+            <Button variant="contained" color="secondary" sx={{mt:2}} onClick={handleSubmit} >
+              Go Pro
+            </Button>
+            </Item>
+          </Grid>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </Grid>
+
+      </Box>
+    </Container>
   );
 }
