9d08338d49fb18449b975664c663a41042b92115
Alex Farouz <alexfarouz@gmail.com>
2024-08-16 15:36:28 -0400

    Update Sidebar

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { Drawer, List, ListItem, ListItemText, Typography, IconButton, Box, Link
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";  // Importing the menu icon from react-icons
 import HomeIcon from '@mui/icons-material/Home';  // Importing the Home icon from MUI
+import { RiAiGenerate } from "react-icons/ri";  // Importing the Generate icon from react-icons/ri
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -53,63 +54,64 @@ const Sidebar = () => {
               <Typography variant="h6" sx={{ fontWeight: 600 }}>Notefy</Typography>
             </ListItem>
 
-            <SignedIn>
-              {/* Image Container */}
+            
               <Box sx={{ 
                 display: 'flex', 
                 flexDirection: 'column', 
                 alignItems: 'center', 
                 position: 'relative',
               }}>
-                {/* Background Image */}
                 <Box
                   component="img"
-                  src="/assets/Flashcards2.jpg"  // Provide the path to the image file
+                  src="/assets/Flashcards2.jpg"
                   alt="Profile Background"
                   sx={{
                     width: 250,
                     height: "auto",
                   }}
                 />
-                
-                <Box sx={{ 
-                  position: 'absolute', 
-                  top: '35%',  // Adjust to position the UserButton
-                  left: '50%', 
-                  transform: 'translate(-50%, -50%) scale(3)',  // Increase the scale value
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  mt: 3
-                }}>
-                  <UserButton 
-                    userProfileMode="modal" 
-                  />
-                  {user && (
-                    <Box sx={{
-                      mt: .25,
-                      px: .5, 
-                      py: 0.125, 
-                      backgroundColor: 'rgba(0, 0, 0, 0.3)',  // Slightly darken the background
-                      borderRadius: '8px', 
-                      backdropFilter: 'blur(4px)',  // Slightly blur the background behind the text
-                    }}>
-                      <Typography sx={{ fontWeight: 600, color: 'white', fontSize: 10 }}>
-                        {user.firstName} {user.lastName}
-                      </Typography>
-                    </Box>
-                  )}
-                </Box>
+                <SignedIn>
+                  <Box sx={{ 
+                    position: 'absolute', 
+                    top: '35%',  // Adjust to position the UserButton
+                    left: '50%', 
+                    transform: 'translate(-50%, -50%) scale(3)',  // Increase the scale value
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    mt: 3
+                  }}>
+                    <UserButton 
+                      userProfileMode="modal" 
+                    />
+                    {user && (
+                      <Box sx={{
+                        mt: .25,
+                        px: .5, 
+                        py: 0.125, 
+                        backgroundColor: 'rgba(0, 0, 0, 0.3)',  // Slightly darken the background
+                        borderRadius: '8px', 
+                        backdropFilter: 'blur(4px)',  // Slightly blur the background behind the text
+                      }}>
+                        <Typography sx={{ fontWeight: 600, color: 'white', fontSize: 10 }}>
+                          {user.firstName} {user.lastName}
+                        </Typography>
+                      </Box>
+                    )}
+                  </Box>
+                </SignedIn>
               </Box>
 
               <ListItem button component="a" href="/" onClick={toggleDrawer}>
                 <HomeIcon sx={{ marginRight: 1.5 }} />
                 <ListItemText primary="Home" sx={{ pt: .5 }}/>
               </ListItem>
-
-              <ListItem button onClick={toggleDrawer}>
-                <ListItemText primary="Generate" />
+            <SignedIn> {/* Only show generate and dashboard when signed in */}
+              <ListItem button component="a" href="/generate" onClick={toggleDrawer}>
+                <Box sx={{ pl: .5, transform: "scale(1.2)"}}><RiAiGenerate /> </Box>
+                <ListItemText primary="Generate" sx={{ pb: .25, ml: 2 }}/>
               </ListItem>
+
               <ListItem button onClick={toggleDrawer}>
                 <ListItemText primary="Dashboard" />
               </ListItem>
