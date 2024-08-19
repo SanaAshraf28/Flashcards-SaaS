424b36532cce3501dfed6864103e8e54e6226fe6
Sana Muhammad Ashraf <ashras17@mcmaster.ca>
2024-08-19 00:02:30 -0400

    commit

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import React, { useRef, useEffect } from "react";
 import { useForm, ValidationError } from '@formspree/react';
 import { useRouter } from "next/navigation";
-import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -152,6 +152,17 @@ export default function Home() {
 
   return (
     <Box className="bg-grid min-h-screen scrollbar">
+      <Head>
+              {/* Google Tag Manager */}
+              dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MTJTHVZN');`,
+          }}
+        {/* End Google Tag Manager --> */}
+      </Head>
       <Box sx={{ position: "fixed", top: 0, right: 0, left: 0, zIndex: 1900 }}>
         <Box 
           sx={{ 
